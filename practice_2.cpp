e7a0c0e3e2c4c73cecc86597592dbf43e28e1785
Pratik_Dhadave <pratik007ddd@gmail.com>
2023-09-19 01:30:36 +0530

    code updated with more examples

---
+++ practice_2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // for (int i=0; i<5; i++){
+    //     cout<<i<<" Babbar"<<endl;
+    // }
+
+    // for(int i=10; i<=12; i++){
+    //     cout<<"Monika ismy darling"<<endl;
+    // }
+
+    // for(int i=10; i>0; i=i-2){
+    //     cout<<i<<endl;
+    // }
+
+    // for(int i=0; i<3; i++){
+    //     cout<<"outer loop "<<i<<endl;
+    //     for(int j=0; j<3; j++){
+    //         cout<<"inner loop "<<j<<endl;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    //  for(;;){   
+    //     int i=1; 
+    //     if(i<5){
+    //         cout<<"Babbar";
+    //         i++;           
+    //     }
+    // }
+
+
+
+    // for(int i=1; i<=11; i++){
+    //     cout<<"Pratik MERN "<<i<<endl;
+    // }
+
+    // int num=19;
+    // for (int i=1; i<=10; i++){
+    //     cout<<num<<"*"<<i<<"="<<num*i<<endl; 
+    // }
+
+    // for (int i=1; i<=10; i++){
+    //     cout<<27*i<<endl; 
+    // }
+
+    for(int i=1; i<=100; i++){
+        if(i%2==0){
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
