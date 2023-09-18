c461b8651d46de38de70dc7e4afd8ed56c693055
Pratik_Dhadave <pratik007ddd@gmail.com>
2023-09-18 21:36:50 +0530

    some basics of if else & for loop

---
+++ practice.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    // int balance;
+    // balance = 12;
+    // if(balance>=10){
+    //     cout<<"maggi..!";
+    // }
+    // else{
+    //     cout<<"kurkure";
+    // }
+
+
+    // int sides;
+    // sides = 3; 
+    // if(sides==3){
+    //     cout<<"it is triangle";
+    // }
+    // else
+    // {
+    //     cout<<"it not triangle";
+    // }
+
+    // int sides=7;
+
+    // if(sides ==3){
+    //     cout<<"it is triangle";
+    // }else if(sides ==4){
+    //     cout<<"it is rectangle";
+    // }else if(sides ==5){
+    //     cout<<"it is pentagon";
+    // }else{
+    //     cout<<"muje nhi pata..";
+    // }
+    
+
+    // int num=-5;
+    // if(num>0){
+    //     cout<<"it is positive no.";
+    // }else if(num<0){
+    //     cout<<"it is negative no.";
+    // }else{
+    //     cout<<"zero";
+    // }
+
+    // int num=25;
+    // if(num%2==1){
+    //     cout<<"odd";
+    // }else{
+    //     cout<<"even";
+    // }
+
+    int age=10;
+    if(age<=10){
+        cout<<"hello ji"<<endl;
+    }
+    cout<<"hii everyone";
+    return 0;
+}
---
+++ prictices_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // for (int i=0; i<5; i++){
+    //     cout<<i<<" Babbar"<<endl;
+    // }
+
+    // for(int i=10; i<=12; i++){
+    //     cout<<"Monika ismy darling"<<endl;
+    // }
+
+    for(int i=10; i>0; i=i-2){
+        cout<<i<<endl;
+    }
+    return 0;
+}
